64de96571553274face3e9eb50b22e2c3f3f601c
Anton Dobrovinskiy <pochtaantona@gmail.com>
2020-07-11 15:40:26 +0300

    Initial Commit

---
+++ coronavirusTracker1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coronavirusTracker1
+//
+//  Created by Антон Добровинский on 11/7/20.
+//  Copyright © 2020 Антон Добровинский. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
